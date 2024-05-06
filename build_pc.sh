8cf93f10acd33263d008a27af819461703aa7799
ilyass <ilyass.ouassou@kleegroup.com>
2024-05-06 09:22:21 +0200

    delting files

--- build_pc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build --build-arg="STAGE=standalone" -t mrc_backend mrc-backend
-docker build --build-arg="STAGE=standalone" -t mrc_nginx mrc-nginx
-docker build --build-arg="STAGE=standalone" -t mrc_frontend mrc-frontend
--- update_pc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker compose -f docker-compose.yml down
-git pull origin main
-git submodule foreach 'git pull origin main'
-./build_pc.sh
-docker compose -f docker-compose.yml up -d
